f302562d42f734a86b405a0ca47c3488a1123a42
Leo Sam <truba666@mail.ru>
2024-08-07 12:16:15 +0500

    added While and Closing the window

--- Timber.cpp
+++ Timber.cpp
@@ -7,7 +7,18 @@ using namespace sf;
 int main()
 {
 	sf::VideoMode vm(1920, 1080);
-	sf::RenderWindow window (vm, "Timber!", Style::Fullscreen);
+	sf::RenderWindow window (vm, "Timber!", sf::Style::Fullscreen);
+
+	while (window.isOpen())
+	{
+
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+		{
+			window.close();
+		}
+
+	}
 
 	return 0;
 }
