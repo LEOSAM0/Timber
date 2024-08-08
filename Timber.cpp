1f564b2a4e2e433aaac918fd9e8bf776c1fea42d
Leo Sam <truba666@mail.ru>
2024-08-08 18:02:14 +0500

    added background.png, spriteBackground.setTexture and spriteBackground.setPosition

--- Timber.cpp
+++ Timber.cpp
@@ -8,6 +8,14 @@ int main()
 {
 	sf::VideoMode vm(1920, 1080);
 	sf::RenderWindow window (vm, "Timber!", sf::Style::Fullscreen);
+	
+	sf::Texture textureBackground;
+	textureBackground.loadFromFile("graphics/background.png");
+
+	sf::Sprite spriteBackground;
+	spriteBackground.setTexture(textureBackground);
+
+	spriteBackground.setPosition(0, 0);
 
 	while (window.isOpen())
 	{
